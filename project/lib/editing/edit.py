145eafa0be9cd6d462a5ec9aea7fb21f4bbccd4f
mrlennix <emmanuel.lennix@mavs.uta.edu>
2017-01-20 13:01:36 -0600

    doc

---
+++ project/lib/editing/edit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+
+class EditController(Object):
+	__init__(self,argv):
+		self.name = name;
+		self.atype = atype;
+
+	def edit(self):
+		pass
+
+start = EditController(sys.argv);
+start.edit();
